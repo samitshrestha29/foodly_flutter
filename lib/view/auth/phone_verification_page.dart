01880fe591ec7e39c0b1ef648b14a1e4166a06d0
samitshrestha29 <samitshrestha29@gmail.com>
2024-05-31 14:15:07 +0545

    updated

--- lib/view/auth/phone_verification_page.dart
+++ lib/view/auth/phone_verification_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:fooodly/common/app_style.dart';
 import 'package:fooodly/constants/constants.dart';
 import 'package:fooodly/controllers/phone_verification_controller.dart';
